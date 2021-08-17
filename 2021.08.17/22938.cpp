3a1acf0d5370628c9d9f0b3ed980ff9ff44787db
nnnlog <sorisem4106@naver.com>
2021-08-17 22:30:18 +0900

    2021.08.17

---
+++ 2021.08.17/22938.cpp
@@ -0,0 +1,29 @@
+#include <bits/stdc++.h>
+
+#define all(v) v.begin(), v.end()
+
+using namespace std;
+
+typedef long long ll;
+typedef unsigned long long ull;
+typedef long double ld;
+typedef pair<int, int> pii;
+typedef pair<ll, ll> pll;
+typedef pair<ld, ld> pld;
+typedef vector<int> vi;
+typedef vector<ll> vl;
+typedef vector<ld> vld;
+typedef vector<pii> vpii;
+typedef vector<pll> vpll;
+typedef vector<pld> vpld;
+typedef unordered_map<int, int> mpii;
+
+int main() {
+	ios_base::sync_with_stdio(false);
+	cin.tie(nullptr);
+	cout.tie(nullptr);
+
+	ll a, b, r1; cin >> a >> b >> r1;
+	ll x, y, r2; cin >> x >> y >> r2;
+	cout << (pow(a - x, 2) + pow(b - y, 2) < pow(r1 + r2, 2) ? "YES" : "NO");
+}
