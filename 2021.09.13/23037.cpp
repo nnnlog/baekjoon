f27697a9cba90ae9d7b53ec2216c8ad0503c8236
nnnlog <sorisem4106@naver.com>
2021-09-13 08:13:01 +0900

    2021.09.13

---
+++ 2021.09.13/23037.cpp
@@ -0,0 +1,35 @@
+#include <bits/stdc++.h>
+
+#define all(v) v.begin(), v.end()
+
+using namespace std;
+
+typedef long long ll;
+typedef unsigned long long ull;
+typedef long double ld;
+typedef pair<int, int> pii;
+typedef pair<ll, ll> pll;
+typedef pair<ld, ld> pld;
+typedef vector<int> vi;
+typedef vector<ll> vl;
+typedef vector<ld> vld;
+typedef vector<pii> vpii;
+typedef vector<pll> vpll;
+typedef vector<pld> vpld;
+typedef unordered_map<int, int> mpii;
+
+int main() {
+	ios_base::sync_with_stdio(false);
+	cin.tie(nullptr);
+	cout.tie(nullptr);
+
+	int n; cin >> n;
+	int ans = 0;
+	while (n) {
+		int a = n % 10;
+		n /= 10;
+
+		ans += a * a * a * a * a;
+	}
+	cout << ans;
+}
