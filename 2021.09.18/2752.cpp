a2541ef7a9ea4222736ad65001296fcf924cf248
nnnlog <sorisem4106@naver.com>
2021-09-18 16:09:16 +0900

    2021.09.18

---
+++ 2021.09.18/2752.cpp
@@ -0,0 +1,30 @@
+#include <bits/stdc++.h>
+
+#define all(v) v.begin(), v.end()
+
+using namespace std;
+
+typedef long long ll;
+typedef unsigned long long ull;
+typedef long double ld;
+typedef pair<int, int> pii;
+typedef pair<ll, ll> pll;
+typedef pair<ld, ld> pld;
+typedef vector<int> vi;
+typedef vector<ll> vl;
+typedef vector<ld> vld;
+typedef vector<pii> vpii;
+typedef vector<pll> vpll;
+typedef vector<pld> vpld;
+typedef unordered_map<int, int> mpii;
+
+int main() {
+	ios_base::sync_with_stdio(false);
+	cin.tie(nullptr);
+	cout.tie(nullptr);
+
+	vi vec(3);
+	for (int &i : vec) cin >> i;
+	sort(all(vec));
+	for (int i : vec) cout << i << " ";
+}
