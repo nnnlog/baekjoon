ae58c49abae7c012eeddb8912a0ef299cb1cf6cf
nnnlog <sorisem4106@naver.com>
2021-12-04 19:56:59 +0900

    2021.12.04

---
+++ 2021.12.04/15025.cpp
@@ -0,0 +1,30 @@
+#include <bits/stdc++.h>
+
+#define all(v) v.begin(), v.end()
+
+using namespace std;
+
+typedef long long ll;
+typedef unsigned long long ull;
+typedef long double ld;
+typedef pair<int, int> pii;
+typedef pair<ll, ll> pll;
+typedef pair<ld, ld> pld;
+typedef vector<int> vi;
+typedef vector<ll> vl;
+typedef vector<ld> vld;
+typedef vector<pii> vpii;
+typedef vector<pll> vpll;
+typedef vector<pld> vpld;
+typedef unordered_map<int, int> mpii;
+
+int main() {
+	ios_base::sync_with_stdio(false);
+	cin.tie(nullptr);
+	cout.tie(nullptr);
+	
+	int a, b; cin >> a >> b;
+    if (!a && !b) cout << "Not a moose";
+    else if (a == b) cout << "Even " << a * 2;
+    else cout << "Odd " << max(a, b) * 2;
+}
