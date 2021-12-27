98e1e0c99c2ec3a7573aa54a50cc602cd54546e1
nnnlog <sorisem4106@naver.com>
2021-12-27 13:16:16 +0900

    2021.12.27

---
+++ 2021.12.27/16693.cpp
@@ -0,0 +1,32 @@
+#include <bits/stdc++.h>
+
+#define all(v) v.begin(), v.end()
+
+using namespace std;
+
+typedef long long ll;
+typedef unsigned long long ull;
+typedef long double ld;
+typedef pair<int, int> pii;
+typedef pair<ll, ll> pll;
+typedef pair<ld, ld> pld;
+typedef vector<int> vi;
+typedef vector<ll> vl;
+typedef vector<ld> vld;
+typedef vector<pii> vpii;
+typedef vector<pll> vpll;
+typedef vector<pld> vpld;
+typedef unordered_map<int, int> mpii;
+
+int main() {
+	ios_base::sync_with_stdio(false);
+	cin.tie(nullptr);
+	cout.tie(nullptr);
+
+	ld a, b, x, y; cin >> a >> b >> x >> y;
+	a /= b;
+	x = x * x * M_PI;
+	x /= y;
+	if (a < x) cout << "Whole pizza";
+	else cout << "Slice of pizza";
+}
