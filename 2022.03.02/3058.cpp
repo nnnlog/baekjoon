0f30f08efd0db738a80d0dae12f0365d20c42b32
nnnlog <sorisem4106@naver.com>
2022-03-02 09:13:11 +0900

    2022.03.02

---
+++ 2022.03.02/3058.cpp
@@ -0,0 +1,51 @@
+#include <bits/stdc++.h>
+
+#define all(v) v.begin(), v.end()
+
+using namespace std;
+
+typedef long long ll;
+typedef unsigned long long ull;
+typedef long double ld;
+typedef pair<int, int> pii;
+typedef pair<ll, ll> pll;
+typedef pair<ld, ld> pld;
+typedef vector<int> vi;
+typedef vector<ll> vl;
+typedef vector<ld> vld;
+typedef vector<pii> vpii;
+typedef vector<pll> vpll;
+typedef vector<pld> vpld;
+typedef unordered_map<int, int> mpii;
+
+void yes() {
+	cout << "YES\n";
+}
+void no() {
+	cout << "NO\n";
+}
+void yes(bool y) {
+	if (y) yes();
+	else no();
+}
+
+int main() {
+	ios_base::sync_with_stdio(false);
+	cin.tie(nullptr);
+	cout.tie(nullptr);
+
+	int T = 1;
+	cin >> T;
+	while (T--) {
+		int n = 7;
+        int sum = 0, mx = 1e9;
+        while (n--) {
+            int a; cin >> a;
+            if (a % 2 == 0) {
+                sum += a;
+                mx = min(mx, a);
+            }
+        }
+        cout << sum << " " << mx << "\n";
+	}
+}
