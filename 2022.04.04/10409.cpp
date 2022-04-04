160ba6b612a7e07c4d2c6e98800dd5809df8fcdc
nnnlog <sorisem4106@naver.com>
2022-04-04 10:08:25 +0900

    2022.04.04

---
+++ 2022.04.04/10409.cpp
@@ -0,0 +1,34 @@
+#include <bits/stdc++.h>
+
+#define all(v) v.begin(), v.end()
+
+using namespace std;
+
+typedef long long ll;
+typedef unsigned long long ull;
+typedef long double ld;
+typedef pair<int, int> pii;
+typedef pair<ll, ll> pll;
+typedef pair<ld, ld> pld;
+typedef vector<int> vi;
+typedef vector<ll> vl;
+typedef vector<ld> vld;
+typedef vector<pii> vpii;
+typedef vector<pll> vpll;
+typedef vector<pld> vpld;
+typedef unordered_map<int, int> mpii;
+
+int main() {
+	ios_base::sync_with_stdio(false);
+	cin.tie(nullptr);
+	cout.tie(nullptr);
+	
+	int n, t; cin >> n >> t;
+    int i;
+    for (i = 1; i <= n; i++) {
+        int a; cin >> a;
+        t -= a;
+        if (t < 0) break;
+    }
+    cout << --i;
+}
