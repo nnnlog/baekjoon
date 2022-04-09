e4da13832ae7bac65de4cca9e532e64722b6ebb1
nnnlog <sorisem4106@naver.com>
2022-04-09 15:12:46 +0900

    2022.04.09

---
+++ 2022.04.09/2863.cpp
@@ -0,0 +1,35 @@
+#include <bits/stdc++.h>
+
+#define all(v) v.begin(), v.end()
+
+using namespace std;
+
+typedef long long ll;
+typedef unsigned long long ull;
+typedef long double ld;
+typedef pair<int, int> pii;
+typedef pair<ll, ll> pll;
+typedef pair<ld, ld> pld;
+typedef vector<int> vi;
+typedef vector<ll> vl;
+typedef vector<ld> vld;
+typedef vector<pii> vpii;
+typedef vector<pll> vpll;
+typedef vector<pld> vpld;
+typedef unordered_map<int, int> mpii;
+
+int main() {
+	ios_base::sync_with_stdio(false);
+	cin.tie(nullptr);
+	cout.tie(nullptr);
+
+	ld a, b, c, d; cin >> a >> b >> c >> d;
+    vpld vec = {
+            {a / c + b / d, 0},
+            {c / d + a / b, 1},
+            {d / b + c / a, 2},
+            {b / a + d / c, 3}
+    };
+    sort(all(vec));
+    cout << vec.back().second;
+}
