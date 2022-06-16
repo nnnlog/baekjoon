5db62ff92f8cccd63bcc854b858a979d81a8678c
nlog <sorisem4106@naver.com>
2022-06-16 13:41:18 +0900

    2022.06.16

---
+++ 2022.06.16/6321.cpp
@@ -0,0 +1,33 @@
+#include <bits/stdc++.h>
+
+#define all(v) v.begin(), v.end()
+
+using namespace std;
+
+typedef long long ll;
+typedef unsigned long long ull;
+typedef long double ld;
+typedef pair<int, int> pii;
+typedef pair<ll, ll> pll;
+typedef pair<ld, ld> pld;
+typedef vector<int> vi;
+typedef vector<ll> vl;
+typedef vector<ld> vld;
+typedef vector<pii> vpii;
+typedef vector<pll> vpll;
+typedef vector<pld> vpld;
+typedef unordered_map<int, int> mpii;
+
+int main() {
+	ios_base::sync_with_stdio(false);
+	cin.tie(nullptr);
+	cout.tie(nullptr);
+	
+	int t; cin >> t;
+    for (int i = 1; i <= t; i++) {
+        string s; cin >> s;
+        cout << "String #" << i << "\n";
+        for (char c : s) if (c < 'Z') cout << char(c + 1); else cout << 'A';
+        cout << "\n\n";
+    }
+}
