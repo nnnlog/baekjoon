4a3f6de4b3a91f22cd45608603e15b3021cdacca
nlog <sorisem4106@naver.com>
2022-06-21 11:00:55 +0900

    2022.06.21

---
+++ 2022.06.21/5612.cpp
@@ -0,0 +1,39 @@
+#include <bits/stdc++.h>
+
+#define all(v) v.begin(), v.end()
+
+using namespace std;
+
+typedef long long ll;
+typedef unsigned long long ull;
+typedef long double ld;
+typedef pair<int, int> pii;
+typedef pair<ll, ll> pll;
+typedef pair<ld, ld> pld;
+typedef vector<int> vi;
+typedef vector<ll> vl;
+typedef vector<ld> vld;
+typedef vector<pii> vpii;
+typedef vector<pll> vpll;
+typedef vector<pld> vpld;
+typedef unordered_map<int, int> mpii;
+
+int main() {
+    ios_base::sync_with_stdio(false);
+    cin.tie(nullptr);
+    cout.tie(nullptr);
+
+    int n; cin >> n;
+    int a = 0, b = 0;
+    for (int i = 0; i <= n; i++) {
+        int x, y = 0;
+        cin >> x;
+        if (i) cin >> y;
+
+        a += x - y;
+        if (a < 0) return cout << 0, 0;
+        b = max(b, a);
+    }
+
+    cout << b;
+}
