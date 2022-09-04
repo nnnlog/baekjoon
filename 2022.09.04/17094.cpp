11c57c6f8d55a7dab034948464b1088c82b07562
nlog <sorisem4106@naver.com>
2022-09-04 09:34:16 +0900

    2022.09.04

---
+++ 2022.09.04/17094.cpp
@@ -0,0 +1,34 @@
+#include <bits/stdc++.h>
+
+#define all(v) v.begin(), v.end()
+
+using namespace std;
+
+typedef long long ll;
+typedef unsigned long long ull;
+typedef long double ld;
+typedef pair<int, int> pii;
+typedef pair<ll, ll> pll;
+typedef pair<ld, ld> pld;
+typedef vector<int> vi;
+typedef vector<ll> vl;
+typedef vector<ld> vld;
+typedef vector<pii> vpii;
+typedef vector<pll> vpll;
+typedef vector<pld> vpld;
+typedef unordered_map<int, int> mpii;
+
+int main() {
+    ios_base::sync_with_stdio(false);
+    cin.tie(nullptr);
+    cout.tie(nullptr);
+
+	int n; cin >> n;
+    int two = 0, e = 0;
+    while (n--) {
+        char c; cin >> c;
+        if (c == '2') two++;
+        else e++;
+    }
+    cout << (two == e ? "yee" : (two > e ? "2" : "e"));
+}
